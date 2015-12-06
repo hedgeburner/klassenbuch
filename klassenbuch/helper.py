374486b6956a346f03e7f35723a22debc8e88449
hedgeburner <nospam@hedgeburner.com>
2015-12-06 13:45:23 +0100

    adding date handling functions

--- klassenbuch/helper.py
+++ klassenbuch/helper.py
@@ -2,8 +2,20 @@
 import datetime
 
 def create_date_list(appstruct):
-    print("Huhu!")
-    return []
+    start = appstruct["start_date"]
+    end = appstruct["end_date"]
+    tag = datetime.timedelta(days=1)
+    holidays = get_holidays(start.year)
+    result = []
+    now = start
+    while now <= end:
+        if is_weekday(now) and (now not in holidays) and
+        not (appstruct["begin_herbst"] <= now <= appstruct["end_herbst"]) and
+        not (appstruct["begin_winter"] <= now <= appstruct["end_winter"]) and
+        not (appstruct["beginn_easter"] <= now <= appstruct["end_easter"]):
+            result.append(now)
+        now += tag
+    return result
     
 def calculate_easter(year):
     """Osteralgorithmus von C. Gauss."""
@@ -19,8 +31,31 @@ def calculate_easter(year):
     Ostern = og + oe
     marz1 = datetime.date(year, 3, 1)
     tag = datetime.timedelta(days=1)
-    for i in range(Ostern-1):
-        marz1 += tag
-    return marz1
+    result = marz1 + Ostern * tag
+    return result
 
-print(calculate_easter(2014))
+def get_holidays(startyear):
+    """calculate all official holidays starting from 1.9
+    of starting year - 31.8. of next year.
+    return a set.
+    """
+    tag = datetime.timedelta(days=1)
+    tag_der_deutschen_einheit = datetime.date(startyear, 10, 3)
+    ostern = calculate_easter(startyear + 1)
+    karfreitag = ostern - 2 * tag
+    ostermontag = ostern + tag
+    mai1 = datetime.date(startyear + 1, 5, 1)
+    himmelfahrt = ostern + 39 * tag
+    pfingsten = ostern + 49 * tag
+    fronleichnam = ostern + 60 * tag
+    return {tag_der_deutschen_einheit, ostern, karfreitag, ostermontag,
+            mai1, himmelfahrt, pfingsten, fronleichnam}
+    
+def is_weekday(date):
+    """
+    return true if the date is a weekday.
+    """
+    return date.isoweekday() < 6
+    
+print(create_date_list({"start_date": datetime.date(2014, 9, 5),
+                        "end_date": datetime.date(2015, 7, 26)}))
